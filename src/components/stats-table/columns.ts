a0c6eee4abfa2981f72d8dc0dde47f8148ea9a23
blicht19 <baileymlicht@gmail.com>
2023-10-07 17:35:03 -0500

    Add utility object that will eventually used in generating power ranking scores

--- src/components/stats-table/columns.ts
+++ src/components/stats-table/columns.ts
@@ -1,5 +1,6 @@
 import { type TableColumn } from 'react-data-table-component';
 import { type TeamAdvancedStats } from '../../types';
+import { statMap } from '../../utils';
 
 export const columns: TableColumn<TeamAdvancedStats>[] = [
   {
@@ -8,7 +9,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     sortable: true,
   },
   {
-    name: 'PPA',
+    name: statMap.offensivePPA.name,
     selector: (row: TeamAdvancedStats) => row.offensivePPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -16,15 +17,15 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Explosiveness',
+    name: statMap.offensiveExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.offensiveExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
-      return b.offensiveExplosiveness - a.defensiveExplosiveness;
+      return b.offensiveExplosiveness - a.offensiveExplosiveness;
     },
   },
   {
-    name: 'Power Success',
+    name: statMap.offensivePowerSuccess.name,
     selector: (row: TeamAdvancedStats) => row.offensivePowerSuccess,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -32,7 +33,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Offense Stuff Rate',
+    name: statMap.offensiveStuffRate.name,
     selector: (row: TeamAdvancedStats) => row.offensiveStuffRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -40,7 +41,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Line Yards',
+    name: statMap.offensiveLineYards.name,
     selector: (row: TeamAdvancedStats) => row.offensiveLineYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -48,7 +49,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Second Level Yards',
+    name: statMap.offensiveSecondLevelYards.name,
     selector: (row: TeamAdvancedStats) => row.offensiveSecondLevelYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -56,7 +57,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Open Field Yards',
+    name: statMap.offensiveOpenFieldYards.name,
     selector: (row: TeamAdvancedStats) => row.offensiveOpenFieldYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -64,7 +65,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Points Per Opportunity',
+    name: statMap.offensivePointsPerOpportunity.name,
     selector: (row: TeamAdvancedStats) => row.offensivePointsPerOpportunity,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -72,7 +73,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Offense Havoc Rate',
+    name: statMap.offensiveHavoc.name,
     selector: (row: TeamAdvancedStats) => row.offensiveHavoc,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -80,7 +81,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing PPA',
+    name: statMap.offensiveRushingPPA.name,
     selector: (row: TeamAdvancedStats) => row.offensiveRushingPPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -88,7 +89,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing Success Rate',
+    name: statMap.offensiveRushingSuccessRate.name,
     selector: (row: TeamAdvancedStats) => row.offensiveRushingSuccessRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -96,15 +97,15 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing Explosiveness',
+    name: statMap.offensiveRushingExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.offensiveRushingExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
-      return b.offensiveExplosiveness - a.offensiveRushingExplosiveness;
+      return b.offensiveRushingExplosiveness - a.offensiveRushingExplosiveness;
     },
   },
   {
-    name: 'Passing PPA',
+    name: statMap.offensivePassingPPA.name,
     selector: (row: TeamAdvancedStats) => row.offensivePassingPPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -112,7 +113,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Passing Success Rate',
+    name: statMap.offensivePassingSuccessRate.name,
     selector: (row: TeamAdvancedStats) => row.offensivePassingSuccessRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -120,15 +121,15 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Passing Explosiveness',
+    name: statMap.offensivePassingExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.offensivePassingExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
-      return b.offensivePassingSuccessRate - a.offensivePassingSuccessRate;
+      return b.offensivePassingExplosiveness - a.offensivePassingExplosiveness;
     },
   },
   {
-    name: 'PPA Allowed',
+    name: statMap.defensivePPA.name,
     selector: (row: TeamAdvancedStats) => row.defensivePPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -136,7 +137,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Success Rate Allowed',
+    name: statMap.defensiveSuccessRate.name,
     selector: (row: TeamAdvancedStats) => row.defensiveSuccessRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -144,7 +145,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Explosiveness Allowed',
+    name: statMap.defensiveExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.defensiveExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -152,7 +153,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Power Success Allowed',
+    name: statMap.defensivePowerSuccess.name,
     selector: (row: TeamAdvancedStats) => row.defensivePowerSuccess,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -160,7 +161,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Defense Stuff Rate',
+    name: statMap.defensiveStuffRate.name,
     selector: (row: TeamAdvancedStats) => row.defensiveStuffRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -168,7 +169,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Line Yards Allowed',
+    name: statMap.defensiveLineYards.name,
     selector: (row: TeamAdvancedStats) => row.defensiveLineYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -176,7 +177,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Second Level Yards Allowed',
+    name: statMap.defensiveSecondLevelYards.name,
     selector: (row: TeamAdvancedStats) => row.defensiveSecondLevelYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -184,7 +185,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Open Field Yards Allowed',
+    name: statMap.defensiveOpenFieldYards.name,
     selector: (row: TeamAdvancedStats) => row.defensiveOpenFieldYards,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -192,7 +193,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Points Per Opportunity Allowed',
+    name: statMap.defensivePointsPerOpportunity.name,
     selector: (row: TeamAdvancedStats) => row.defensivePointsPerOpportunity,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -200,7 +201,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Defense Havoc Rate',
+    name: statMap.defensiveHavoc.name,
     selector: (row: TeamAdvancedStats) => row.defensiveHavoc,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -208,7 +209,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing PPA Allowed',
+    name: statMap.defensiveRushingPPA.name,
     selector: (row: TeamAdvancedStats) => row.defensiveRushingPPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -216,7 +217,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing Success Rate Allowed',
+    name: statMap.defensiveRushingSuccessRate.name,
     selector: (row: TeamAdvancedStats) => row.defensiveRushingSuccessRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -224,7 +225,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Rushing Explosiveness Allowed',
+    name: statMap.defensiveRushingExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.defensiveRushingExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -232,7 +233,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Passing PPA Allowed',
+    name: statMap.defensivePassingPPA.name,
     selector: (row: TeamAdvancedStats) => row.defensivePassingPPA,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -240,7 +241,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Passing Success Rate Allowed',
+    name: statMap.defensivePassingSuccessRate.name,
     selector: (row: TeamAdvancedStats) => row.defensivePassingSuccessRate,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
@@ -248,7 +249,7 @@ export const columns: TableColumn<TeamAdvancedStats>[] = [
     },
   },
   {
-    name: 'Passing Explosiveness Allowed',
+    name: statMap.defensivePassingExplosiveness.name,
     selector: (row: TeamAdvancedStats) => row.defensivePassingExplosiveness,
     sortable: true,
     sortFunction: (a: TeamAdvancedStats, b: TeamAdvancedStats) => {
---
+++ src/utils/stat-map.ts
@@ -0,0 +1,126 @@
+export const statMap = {
+  offensivePPA: {
+    name: 'PPA',
+    positive: true,
+  },
+  offensiveExplosiveness: {
+    name: 'Explosiveness',
+    positive: true,
+  },
+  offensivePowerSuccess: {
+    name: 'Power Success',
+    positive: true,
+  },
+  offensiveStuffRate: {
+    name: 'Offensive Stuff Rate',
+    positive: false,
+  },
+  offensiveLineYards: {
+    name: 'Line Yards',
+    positive: true,
+  },
+  offensiveSecondLevelYards: {
+    name: 'Second Level Yards',
+    positive: true,
+  },
+  offensiveOpenFieldYards: {
+    name: 'Open Field Yards',
+    positive: true,
+  },
+  offensivePointsPerOpportunity: {
+    name: 'Points Per Opportunity',
+    positive: true,
+  },
+  offensiveHavoc: {
+    name: 'Offensive Havoc Rate',
+    positive: false,
+  },
+  offensiveRushingPPA: {
+    name: 'Rushing PPA',
+    positive: true,
+  },
+  offensiveRushingSuccessRate: {
+    name: 'Rushing Success Rate',
+    positive: true,
+  },
+  offensiveRushingExplosiveness: {
+    name: 'Rushing Explosiveness',
+    positive: true,
+  },
+  offensivePassingPPA: {
+    name: 'Passing PPA',
+    positive: true,
+  },
+  offensivePassingSuccessRate: {
+    name: 'Passing Success Rate',
+    positive: true,
+  },
+  offensivePassingExplosiveness: {
+    name: 'Passing Explosiveness',
+    positive: true,
+  },
+  defensivePPA: {
+    name: 'PPA Allowd',
+    positive: false,
+  },
+  defensiveSuccessRate: {
+    name: 'Success Rate Allowed',
+    positive: false,
+  },
+  defensiveExplosiveness: {
+    name: 'Explosiveness Allowed',
+    positive: false,
+  },
+  defensivePowerSuccess: {
+    name: 'Power Success Allowed',
+    positive: false,
+  },
+  defensiveStuffRate: {
+    name: 'Defensive Stuff Rate',
+    positive: true,
+  },
+  defensiveLineYards: {
+    name: 'Line Yards Allowed',
+    positive: false,
+  },
+  defensiveSecondLevelYards: {
+    name: 'Second Level Yards Allowd',
+    positive: false,
+  },
+  defensiveOpenFieldYards: {
+    name: 'Open Field Yards Allowed',
+    positive: false,
+  },
+  defensivePointsPerOpportunity: {
+    name: 'Points Per Opportunity Allowed',
+    positive: false,
+  },
+  defensiveHavoc: {
+    name: 'Defensive Havoc Rate',
+    positive: true,
+  },
+  defensiveRushingPPA: {
+    name: 'Rushing PPA Allowed',
+    positive: false,
+  },
+  defensiveRushingSuccessRate: {
+    name: 'Rushing Success Rate Allowed',
+    positive: false,
+  },
+  defensiveRushingExplosiveness: {
+    name: 'Rushing Explosiveness Allowed',
+    positive: false,
+  },
+  defensivePassingPPA: {
+    name: 'Passing PPA Allowed',
+    postive: false,
+  },
+  defensivePassingSuccessRate: {
+    name: 'Passing Success Rate Allowed',
+    positive: false,
+  },
+  defensivePassingExplosiveness: {
+    name: 'Passing Explosiveness Allowed',
+    positive: false,
+  },
+};
